2f94279caca60a6c4289f27608e7af3e18c47826
Daniel Marklund <daniel@recurringly.com>
2011-08-11 11:26:39 +0200

    Tweet text malfunction now resolved.

--- views/elements/social-media.php
+++ views/elements/social-media.php
@@ -1,3 +1,6 @@
 <?php namespace melt; ?>
+<?php $tweet_text= urlencode("Check out my brand new Melt application");?>
+
+
 <a href="http://www.facebook.com/meltframework" title="Visit Melt on facebook"><img src="<?php echo url('static/images/icon-facebook.png'); ?>" alt="Visit melt on facebook" /></a>
-<a href="http://twitter.com/share" data-text="Check out my brand new Melt application" data-count="none" title="Tweet about you application"><img src="<?php echo url('static/images/icon-twitter.png'); ?>" alt="Tweet about your application" /></a><script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
+<a href="http://twitter.com/share?&text=<?php echo $tweet_text; ?>" data-text="Check out my brand new Melt application" data-count="none" title="Tweet about you application"><img src="<?php echo url('static/images/icon-twitter.png'); ?>" alt="Tweet about your application" /></a>
